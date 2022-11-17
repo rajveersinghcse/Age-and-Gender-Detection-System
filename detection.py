95c3aea7b762d96d759655853eb14483e8d8e201
Rajveer Singh <117706744+rajveersinghcse@users.noreply.github.com>
2022-11-17 19:58:03 +0530

    new

--- detection.py
+++ detection.py
@@ -13,9 +13,9 @@ ageModel = "age_net.caffemodel"
 genderProto = "gender_deploy.prototxt"
 genderModel = "gender_net.caffemodel"
 
-faceNet=cv2.dnn.readNet(faceModel, faceProto)
-ageNet=cv2.dnn.readNet(ageModel,ageProto)
-genderNet=cv2.dnn.readNet(genderModel,genderProto)
+faceNet=cv.dnn.readNet(faceModel, faceProto)
+ageNet=cv.dnn.readNet(ageModel,ageProto)
+genderNet=cv.dnn.readNet(genderModel,genderProto)
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
@@ -27,7 +27,7 @@ padding=20
 def faceBox(faceNet, frames):
     frameHeight=frames.shape[0]
     frameWidth=frames.shape[1]
-    blob=cv2.dnn.blobFromImage(frames, 1.0, (300,300), [104,117,123], swapRB=False)
+    blob=cv.dnn.blobFromImage(frames, 1.0, (300,300), [104,117,123], swapRB=False)
     faceNet.setInput(blob)
     detection=faceNet.forward()
     bboxs=[]
@@ -39,29 +39,29 @@ def faceBox(faceNet, frames):
             x2=int(detection[0,0,i,5]*frameWidth)
             y2=int(detection[0,0,i,6]*frameHeight)
             bboxs.append([x1,y1,x2,y2])
-            cv2.rectangle(frames, (x1,y1),(x2,y2),(0,255,0), 1)
+            cv.rectangle(frames, (x1,y1),(x2,y2),(0,255,0), 1)
     return frames, bboxs
 
 """ Video display """
 
 def DisplayVid():
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('testvideo', fourcc, 20.0, (640, 480))
+    cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    out = cv.VideoWriter('testvideo', fourcc, 20.0, (640, 480))
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
 
     while (True):
         ret, frame = cap.read()
         frameFace, bboxes = faceBox(faceNet, frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         out.write(frame)
 
         for bbox in bboxes:
             face = frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
 
-            blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
             genderNet.setInput(blob)
             genderPreds = genderNet.forward()
             gender = genderList[genderPreds[0].argmax()]
@@ -71,12 +71,12 @@ def DisplayVid():
             age = ageList[agePreds[0].argmax()]
 
             label = "{},{}".format(gender, age)
-            cv2.rectangle(frameFace, (bbox[0], bbox[1] - 30), (bbox[2], bbox[1]), (0, 255, 0), -1)
-            cv2.putText(frameFace, label, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2,
-                        cv2.LINE_AA)
-        cv2.imshow("Age-Gender", frameFace)
-        k = cv2.waitKey(1)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv.rectangle(frameFace, (bbox[0], bbox[1] - 30), (bbox[2], bbox[1]), (0, 255, 0), -1)
+            cv.putText(frameFace, label, (bbox[0], bbox[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2,
+                        cv.LINE_AA)
+        cv.imshow("Age-Gender", frameFace)
+        k = cv.waitKey(1)
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
         if not (cap.isOpened()):
@@ -84,6 +84,6 @@ def DisplayVid():
 
     cap.release()
     out.release()
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
 
 DisplayVid()
